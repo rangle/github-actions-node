2aea815db8f2d2063be9a0b1e7443b07f770294e
roofus <roofus@roofuss-mbp.lan>
2022-01-31 17:03:34 -0500

    updated github-actions.yml

--- services/booksService.spec.js
+++ services/booksService.spec.js
@@ -78,7 +78,7 @@ describe('test getBooks', () => {
   it('saves successfully if req.body data is valid', async () => {
     //have to connect to mongoose otherwise the async call book.save() will hang
     await mongoose
-      .connect('mongodb://mongodb:27017/test', {
+      .connect('mongodb://127.0.0.1:27017/test', {
         dbName: 'sample-db',
       })
       .then(() => {
