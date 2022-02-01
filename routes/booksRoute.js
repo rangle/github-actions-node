4836804bbd542455daba96e4458facd50c7bcd50
roofus <roofus@roofuss-mbp.lan>
2022-02-01 13:29:30 -0500

    fixed error

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -12,7 +12,7 @@ router.use((req, res, next) => {
 });
 
 router.use('/latest', (req, res, next) => {
-  console.log('Middleware called on books/latest
+  console.log('Middleware called on books/latest route ');
   next();
 });
 
